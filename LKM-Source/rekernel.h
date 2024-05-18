c96bfa52ba933116912bda171f7ec2b249e3ca2d
YuSaki丶Kanade <53483352+Nep-Timeline@users.noreply.github.com>
2024-05-18 16:02:21 +0800

    Create rekernel.h

---
+++ LKM-Source/rekernel.h
@@ -0,0 +1,16 @@
+#ifndef FREEZER_GKI_H
+#define FREEZER_GKI_H
+
+// define on your own
+// #define KERNEL_5_10
+// #define KERNEL_5_15
+// #define KERNEL_6_1
+
+#define MIN_USERAPP_UID (10000)
+#define MAX_SYSTEM_UID  (2000)
+#define RESERVE_ORDER					17
+#define WARN_AHEAD_SPACE				(1 << RESERVE_ORDER)
+#define LINE_ERROR (-1)
+#define LINE_SUCCESS (0)
+
+#endif
