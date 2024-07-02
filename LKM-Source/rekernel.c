207c1b518b8160c4f4906b565f6db3af2c19a49b
lzghzr <me@lzzr.me>
2024-07-03 00:32:14 +0800

    free the outdated transaction and buffer

--- LKM-Source/rekernel.c
+++ LKM-Source/rekernel.c
@@ -41,6 +41,8 @@
 #include <linux/types.h>
 #include <linux/slab.h>
 
+#include <linux/kprobes.h>
+
 #define NETLINK_REKERNEL_MAX     		26
 #define NETLINK_REKERNEL_MIN     		22
 #define USER_PORT        				100
@@ -50,6 +52,11 @@ char netlink_kmsg[PACKET_SIZE];
 struct sock *netlink_socket = NULL;
 extern struct net init_net;
 int netlink_unit = NETLINK_REKERNEL_MIN;
+
+static unsigned long (*re_kallsyms_lookup_name)(const char* name);
+static void (*re_binder_transaction_buffer_release)(struct binder_proc* proc, struct binder_thread* thread, struct binder_buffer* buffer, binder_size_t off_end_offset, bool is_failure);
+static void (*re_binder_alloc_free_buf)(struct binder_alloc* alloc, struct binder_buffer* buffer);
+static struct binder_stats(*re_binder_stats);
 
 /* hashmap for monitored uids */
 #define REKERNEL_UID_HASH_BITS 6
@@ -263,43 +270,117 @@ void line_binder_transaction(void *data, struct binder_proc *target_proc, struct
 	}
 }
 
-#ifndef KERNEL_5_10
-
-void line_binder_proc_transaction_entry(void* data, struct binder_proc* proc, struct binder_transaction* t,
-	struct binder_thread** thread, int node_debug_id, bool pending_async, bool sync, bool* skip)
+static inline void binder_inner_proc_lock(struct binder_proc* proc)
+__acquires(&proc->inner_lock)
 {
-	if (sync)
-		return;
-	if (proc->is_frozen) {
-#ifdef DEBUG
-		pr_info("[Re-Kernel LKM] Filter Binder! proc=%d\n", task_uid(proc->tsk).val);
-#endif
-		t->flags |= TF_UPDATE_TXN;
-		return;
+	spin_lock(&proc->inner_lock);
+}
+
+static inline void binder_inner_proc_unlock(struct binder_proc* proc)
+__releases(&proc->inner_lock)
+{
+	spin_unlock(&proc->inner_lock);
+}
+
+static inline void binder_node_lock(struct binder_node* node)
+__acquires(&node->lock)
+{
+	spin_lock(&node->lock);
+}
+
+static inline void binder_node_unlock(struct binder_node* node)
+__releases(&node->lock)
+{
+	spin_unlock(&node->lock);
+}
+
+static bool binder_can_update_transaction(struct binder_transaction* t1, struct binder_transaction* t2)
+{
+	if ((t1->flags & t2->flags & TF_ONE_WAY) != TF_ONE_WAY || !t1->to_proc || !t2->to_proc)
+		return false;
+	if (t1->to_proc->tsk == t2->to_proc->tsk && t1->code == t2->code &&
+		t1->flags == t2->flags && t1->buffer->pid == t2->buffer->pid &&
+		t1->buffer->target_node->ptr == t2->buffer->target_node->ptr &&
+		t1->buffer->target_node->cookie == t2->buffer->target_node->cookie)
+		return true;
+	return false;
+}
+
+static struct binder_transaction* binder_find_outdated_transaction_ilocked(struct binder_transaction* t,
+	struct list_head* target_list)
+{
+	struct binder_work* w;
+
+	list_for_each_entry(w, target_list, entry) {
+		struct binder_transaction* t_queued;
+
+		if (w->type != BINDER_WORK_TRANSACTION)
+			continue;
+		t_queued = container_of(w, struct binder_transaction, work);
+		if (binder_can_update_transaction(t_queued, t))
+			return t_queued;
 	}
+	return NULL;
+}
+
+static inline void __nocfi binder_release_entire_buffer(struct binder_proc* proc,
+	struct binder_thread* thread, struct binder_buffer* buffer, bool is_failure)
+{
+	binder_size_t off_end_offset;
+
+	off_end_offset = ALIGN(buffer->data_size, sizeof(void*));
+	off_end_offset += buffer->offsets_size;
+
+	re_binder_transaction_buffer_release(proc, thread, buffer,
+		off_end_offset, is_failure);
+}
+
+static inline void binder_stats_deleted(enum binder_stat_types type)
+{
+	atomic_inc(&re_binder_stats->obj_deleted[type]);
+}
+
+static int __nocfi binder_proc_transaction_pre(struct kprobe* p, struct pt_regs* regs)
+{
+	struct binder_transaction* t = (struct binder_transaction*)regs->regs[0];
+	struct binder_proc* proc = (struct binder_proc*)regs->regs[1];
+
+	struct binder_node* node = NULL;
+	struct binder_transaction* t_outdated = NULL;
+
+	if (!proc || proc->is_frozen || !(t->flags & TF_ONE_WAY))
+		return 0;
+
 	if (line_is_frozen(proc->tsk)) {
-#ifdef DEBUG
-		pr_info("[Re-Kernel LKM] Filter Binder! proc=%d\n", task_uid(proc->tsk).val);
-#endif
-		t->flags |= TF_UPDATE_TXN;
-		proc->is_frozen = true;
-		proc->sync_recv = true;
-	}
-}
+		node = t->buffer->target_node;
+		if (!node)
+			return 0;
 
-void line_binder_transaction_finish(void* data, struct binder_proc* proc, struct binder_transaction* t,
-	struct task_struct* binder_th_task, bool pending_async, bool sync)
-{
-	if (!sync && proc->sync_recv) {
-#ifdef DEBUG
-		pr_info("[Re-Kernel LKM] Filter Binder! proc=%d\n", task_uid(proc->tsk).val);
-#endif
-		proc->is_frozen = false;
-		proc->sync_recv = false;
-	}
-}
+		binder_node_lock(node);
+		binder_inner_proc_lock(proc);
+		t_outdated = binder_find_outdated_transaction_ilocked(t, &node->async_todo);
+		if (t_outdated) {
+			list_del_init(&t_outdated->work.entry);
+			proc->outstanding_txns--;
+		}
+		binder_inner_proc_unlock(proc);
+		binder_node_unlock(node);
 
+		if (t_outdated) {
+			struct binder_buffer* buffer = t_outdated->buffer;
+#ifdef DEBUG
+			pr_info("[Re-Kernel LKM] free_outdated txn %d supersedes %d\n", t->debug_id, t_outdated->debug_id);
 #endif
+			t_outdated->buffer = NULL;
+			buffer->transaction = NULL;
+			binder_release_entire_buffer(proc, NULL, buffer, false);
+			re_binder_alloc_free_buf(&proc->alloc, buffer);
+			kfree(t_outdated);
+			binder_stats_deleted(BINDER_STAT_TRANSACTION);
+		}
+	}
+	return 0;
+}
 
 void line_signal(void *data, int sig, struct task_struct *killer, struct task_struct *dst)
 {
@@ -345,18 +426,6 @@ int register_binder(void)
 		pr_err("register_trace_android_vh_binder_trans failed, rc=%d\n", rc);
 		return rc;
 	}
-#ifndef KERNEL_5_10
-	rc = register_trace_android_vh_binder_proc_transaction_entry(line_binder_proc_transaction_entry, NULL);
-	if (rc != LINE_SUCCESS) {
-		pr_err("register_trace_android_vh_binder_proc_transaction_entry failed, rc=%d\n", rc);
-		return rc;
-	}
-	rc = register_trace_android_vh_binder_proc_transaction_finish(line_binder_transaction_finish, NULL);
-	if (rc != LINE_SUCCESS) {
-		pr_err("register_trace_android_vh_binder_proc_transaction_finish failed, rc=%d\n", rc);
-		return rc;
-	}
-#endif
 	return LINE_SUCCESS;
 }
 
@@ -366,10 +435,6 @@ void unregister_binder(void)
 	unregister_trace_android_vh_binder_preset(line_binder_preset, NULL);
 	unregister_trace_android_vh_binder_reply(line_binder_reply, NULL);
 	unregister_trace_android_vh_binder_trans(line_binder_transaction, NULL);
-#ifndef KERNEL_5_10
-	unregister_trace_android_vh_binder_proc_transaction_entry(line_binder_proc_transaction_entry, NULL);
-	unregister_trace_android_vh_binder_proc_transaction_finish(line_binder_transaction_finish, NULL);
-#endif
 }
 
 int register_signal(void)
@@ -576,6 +641,48 @@ static const struct proc_ops rekernel_unit_fops = {
 	.proc_release   = single_release,
 };
 
+static struct kprobe kp_kallsyms_lookup_name = {
+	.symbol_name = "kallsyms_lookup_name"
+};
+static struct kprobe kp_binder_proc_transaction = {
+	.symbol_name = "binder_proc_transaction",
+	.pre_handler = binder_proc_transaction_pre
+};
+
+int __nocfi register_kp(void) {
+	int rc = LINE_SUCCESS;
+
+	rc = register_kprobe(&kp_kallsyms_lookup_name);
+	if (rc != LINE_SUCCESS) {
+		pr_err("register kprobe hooks failed, rc=%d\n", rc);
+		return rc;
+	}
+	re_kallsyms_lookup_name = (void*)kp_kallsyms_lookup_name.addr;
+	unregister_kprobe(&kp_kallsyms_lookup_name);
+
+	re_binder_transaction_buffer_release = (void*)re_kallsyms_lookup_name("binder_transaction_buffer_release");
+	re_binder_alloc_free_buf = (void*)re_kallsyms_lookup_name("binder_alloc_free_buf");
+	re_binder_stats = (void*)re_kallsyms_lookup_name("binder_stats");
+
+	if (re_binder_transaction_buffer_release == NULL || re_binder_alloc_free_buf == NULL || re_binder_stats == NULL) {
+		rc = -EINVAL;
+		pr_err("register kprobe kallsyms_lookup_name failed, rc=%d\n", rc);
+		return rc;
+	}
+
+	register_kprobe(&kp_binder_proc_transaction);
+	if (rc != LINE_SUCCESS) {
+		pr_err("register binder_proc_transaction hooks failed, rc=%d\n", rc);
+		return rc;
+	}
+
+	return LINE_SUCCESS;
+}
+
+void unregister_kp(void) {
+	unregister_kprobe(&kp_binder_proc_transaction);
+}
+
 static struct proc_dir_entry *rekernel_dir, *rekernel_unit_entry;    
 
 static int __init start_rekernel(void)
@@ -629,6 +736,11 @@ static int __init start_rekernel(void)
 		return LINE_ERROR;
 	}
 
+	if (register_kp() != LINE_SUCCESS) {
+		pr_err("%s: Failed to hook kprobe!\n", __func__);
+		return LINE_ERROR;
+	}
+
 	pr_info("Re-Kernel hooked!\n");
 	return LINE_SUCCESS;
 }
@@ -639,6 +751,7 @@ static void __exit exit_rekernel(void)
 	unregister_binder();
 	unregister_signal();
 	unregister_netfilter();
+	unregister_kp();
 	netlink_kernel_release(netlink_socket);
 }
 
