f733b15a0c37e53372ff24ef91097017e04be54e
YuSaki丶Kanade <53483352+Nep-Timeline@users.noreply.github.com>
2024-07-09 15:23:27 +0800

    Update rekernel.h

--- LKM-Source/rekernel.h
+++ LKM-Source/rekernel.h
@@ -6,12 +6,12 @@
 // #define KERNEL_5_15
 // #define KERNEL_6_1
 
-#define NETWORK_FILTER
-
 #define MIN_USERAPP_UID (10000)
 #define MAX_SYSTEM_UID  (2000)
 #define RESERVE_ORDER					17
 #define WARN_AHEAD_SPACE				(1 << RESERVE_ORDER)
+#define INTERFACETOKEN_BUFF_SIZE (140)
+#define PARCEL_OFFSET (16) /* sync with the writeInterfaceToken */
 #define LINE_ERROR (-1)
 #define LINE_SUCCESS (0)
 
