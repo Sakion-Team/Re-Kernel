a49c49f92d5442dfa7914b2b1c476342c78c00bc
YuSaki丶Kanade <53483352+Nep-Timeline@users.noreply.github.com>
2025-06-11 11:23:46 +0800

    Added SYSTEM_APP_UID define

--- LKM-Source/rekernel.h
+++ LKM-Source/rekernel.h
@@ -8,13 +8,14 @@
 // #define KERNEL_6_6
 // #define KERNEL_6_12
 
-#define MIN_USERAPP_UID (10000)
-#define MAX_SYSTEM_UID  (2000)
-#define RESERVE_ORDER					17
-#define WARN_AHEAD_SPACE				(1 << RESERVE_ORDER)
-#define INTERFACETOKEN_BUFF_SIZE (140)
-#define PARCEL_OFFSET (16) /* sync with the writeInterfaceToken */
-#define LINE_ERROR (-1)
-#define LINE_SUCCESS (0)
+#define MIN_USERAPP_UID                 (10000)
+#define MAX_SYSTEM_UID                  (2000)
+#define SYSTEM_APP_UID                  (1000)
+#define RESERVE_ORDER					          17
+#define WARN_AHEAD_SPACE				        (1 << RESERVE_ORDER)
+#define INTERFACETOKEN_BUFF_SIZE        (140)
+#define PARCEL_OFFSET                   (16) /* sync with the writeInterfaceToken */
+#define LINE_ERROR                      (-1)
+#define LINE_SUCCESS                    (0)
 
 #endif
