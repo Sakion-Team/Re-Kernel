1d0201085363e0dc30f142940b616b3bdba83263
YuSaki丶Kanade <53483352+Nep-Timeline@users.noreply.github.com>
2025-06-11 11:28:24 +0800

    Added CLEAN_UP_ASYNC_BINDER, CPUCTL_LISTEN define

--- LKM-Source/rekernel.h
+++ LKM-Source/rekernel.h
@@ -8,11 +8,16 @@
 // #define KERNEL_6_6
 // #define KERNEL_6_12
 
+// define on your own
+// #define CPUCTL_LISTEN
+
+#define CLEAN_UP_ASYNC_BINDER
+
 #define MIN_USERAPP_UID                 (10000)
 #define MAX_SYSTEM_UID                  (2000)
 #define SYSTEM_APP_UID                  (1000)
-#define RESERVE_ORDER					          17
-#define WARN_AHEAD_SPACE				        (1 << RESERVE_ORDER)
+#define RESERVE_ORDER					17
+#define WARN_AHEAD_SPACE				(1 << RESERVE_ORDER)
 #define INTERFACETOKEN_BUFF_SIZE        (140)
 #define PARCEL_OFFSET                   (16) /* sync with the writeInterfaceToken */
 #define LINE_ERROR                      (-1)
