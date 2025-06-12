c3e3b8e2058e3ec0b9b54cee70f5d19ad75b1812
YuSaki丶Kanade <53483352+Nep-Timeline@users.noreply.github.com>
2025-06-12 16:36:17 +0800

    Update ReKernel.java

--- Develop/sample/java/ReKernel.java
+++ Develop/sample/java/ReKernel.java
@@ -1,116 +1,115 @@
-package nep.timeline.freezer.core.kernel;
-
-import android.system.ErrnoException;
-
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.net.SocketAddress;
-import java.net.SocketException;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-
-import nep.timeline.freezer.core.kernel.netlink.NetlinkClient;
-import nep.timeline.freezer.core.kernel.netlink.NetlinkSocketAddress;
-import nep.timeline.freezer.core.log.Log;
-import nep.timeline.freezer.core.utils.StringUtils;
-
 public class ReKernel {
     private static boolean isRunning = false;
-    public static boolean received = false;
     private static final int NETLINK_UNIT_DEFAULT = 22;
+    private static final int NETLINK_UNIT_MAX = 26;
+    private static final Handler rekernel = new Handler(new HandlerThread("ReKernel").getLooper());
+    private static final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-    // You should create a new thread to invoke this method
+    private static Map<String, String> parseParams(String message) {
+        Map<String, String> map = new HashMap<>();
+        for (String keyValue : message.split(",")) {
+            String[] split = keyValue.split("=");
+            if (split.length == 2)
+                map.put(split[0].trim(), split[1].trim());
+        }
+        return map;
+    }
+
+    private static int StringToInteger(String str) {
+        String data = str.trim();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < data.length(); i++) {
+            char c = data.charAt(i);
+            if (Character.isDigit(c))
+                result.append(c);
+        }
+
+        try {
+            return Integer.parseInt(result.toString());
+        } catch (NumberFormatException ignored) {
+            return -1;
+        }
+    }
+
     public static void start() {
         if (isRunning)
             return;
 
-        try {
-            int netlinkUnit;
-            File file = new File(FreezerConfig.ConfigDir, "netlink.unit");
-            if (file.exists()) {
-                String unit = FreezerConfig.getString("netlink.unit");
-                if (unit.trim().isEmpty()) {
+        executorService.execute(() -> {
+            try {
+                int netlinkUnit;
+                int configNetlinkUnit = Settings.netlinkUnit;
+                if (configNetlinkUnit >= NETLINK_UNIT_DEFAULT && configNetlinkUnit <= NETLINK_UNIT_MAX) {
+                    netlinkUnit = configNetlinkUnit;
+                } else {
                     File dir = new File("/proc/rekernel");
                     if (dir.exists()) {
                         File[] files = dir.listFiles();
                         if (files == null) {
-                            Log.e("Failed to find re:kernel unit");
+                            // 找不到ReKernel单元
                             return;
                         }
                         File unitFile = files[0];
-                        netlinkUnit = StringUtils.StringToInteger(unitFile.getName());
+                        netlinkUnit = StringToInteger(unitFile.getName());
                     } else netlinkUnit = NETLINK_UNIT_DEFAULT;
-                } else {
-                    netlinkUnit = StringUtils.StringToInteger(unit);
                 }
-            } else {
-                File dir = new File("/proc/rekernel");
-                if (dir.exists()) {
-                    File[] files = dir.listFiles();
-                    if (files == null) {
-                        Log.e("Failed to find re:kernel unit");
+
+                try (NetlinkClient netlinkClient = new NetlinkClient(netlinkUnit)) {
+                    if (!netlinkClient.getMDescriptor().valid()) {
+                        // 连接失败
                         return;
                     }
-                    File unitFile = files[0];
-                    netlinkUnit = StringUtils.StringToInteger(unitFile.getName());
-                } else netlinkUnit = NETLINK_UNIT_DEFAULT;
-            }
 
-            NetlinkClient netlinkClient = new NetlinkClient(netlinkUnit);
-            if (!netlinkClient.getmDescriptor().valid()) {
-                Log.e("Failed to connect re:kernel server");
-                return;
-            }
+                    netlinkClient.bind((SocketAddress) new NetlinkSocketAddress(100).toInstance());
 
-            netlinkClient.bind((SocketAddress) new NetlinkSocketAddress(100).toInstance());
+                    isRunning = true;
 
-            isRunning = true;
+                    // 连接成功
 
-            Log.i("Connected to Re:Kernel!");
+                    while (true) {
+                        try {
+                            ByteBuffer byteBuffer = netlinkClient.recvMessage();
+                            String data = new String(byteBuffer.array(), byteBuffer.position(), byteBuffer.limit(), StandardCharsets.UTF_8);
+                            if (!data.isEmpty()) {
+                                Map<String, String> params = parseParams(data.substring(data.indexOf("type"), data.lastIndexOf(";")));
+                                rekernel.post(() -> {
+                                    String type = params.get("type");
+                                    if (type.equals("Binder")) {
+                                        String bindertype = params.get("bindertype");
+                                        int oneway = StringToInteger(params.get("oneway"));
+                                        int fromPid = StringToInteger(params.get("from_pid"));
+                                        int fromUid = StringToInteger(params.get("from"));
+                                        int targetPid = StringToInteger(params.get("target_pid"));
+                                        int targetUid = StringToInteger(params.get("target"));
+                                        int rpcName = params.get("rpc_name");
+                                        int code = StringToInteger(params.get("code"));
+                                        // 你的代码
+                                    } else if (type.equals("Signal")) {
+                                        int targetPid = StringToInteger(params.get("dst_pid"));
+                                        int targetUid = StringToInteger(params.get("dst"));
+                                        int killerPid = StringToInteger(params.get("killer_pid"));
+                                        int killerUid = StringToInteger(params.get("killer"));
+                                        int signal = StringToInteger(params.get("signal"));
+                                        // 你的代码
+                                    } else if (type.equals("Network")) {
+                                        int targetUid = StringToInteger(params.get("target"));
+                                        String proto = params.get("proto");
+                                        // 你的代码
+                                    }
+                                });
+                            }
+                        } catch (ErrnoException | InterruptedIOException | NumberFormatException ignored) {
 
-            while (true) {
-                try {
-                    ByteBuffer byteBuffer = netlinkClient.recvMessage();
-                    String data = new String(byteBuffer.array(), byteBuffer.position(), byteBuffer.limit(), StandardCharsets.UTF_8);
-                    if (!data.isEmpty()) {
-                        if (data.contains("type=") && !received) {
-                            Log.i("Successfully received message from re:kernel");
-                            received = true;
-                        }
-                        String type = StringUtils.getSubString(data, "type=", ",").trim();
-                        if (type.equals("Binder")) {
-                            String bindertype = StringUtils.getSubString(data, "bindertype=", ",").trim();
-                            int oneway = StringUtils.StringToInteger(StringUtils.getSubString(data, "oneway=", ","));
-                            int fromPid = StringUtils.StringToInteger(StringUtils.getSubString(data, "from_pid=", ","));
-                            int fromUid = StringUtils.StringToInteger(StringUtils.getSubString(data, "from=", ","));
-                            int targetPid = StringUtils.StringToInteger(StringUtils.getSubString(data, "target_pid=", ","));
-                            int targetUid = StringUtils.StringToInteger(StringUtils.getSubString(data, "target=", ","));
-                            int rpcName = StringUtils.getSubString(data, "rpc_name=", ",");
-                            int code = StringUtils.StringToInteger(StringUtils.getSubString(data, "code=", ";"));
-                            // Your code
-                        } else if (type.equals("Signal")) {
-                            int killerPid = StringUtils.StringToInteger(StringUtils.getSubString(data, "killer_pid=", ","));
-                            int killerUid = StringUtils.StringToInteger(StringUtils.getSubString(data, "killer=", ","));
-                            int targetPid = StringUtils.StringToInteger(StringUtils.getSubString(data, "dst_pid=", ","));
-                            int targetUid = StringUtils.StringToInteger(StringUtils.getSubString(data, "dst=", ";"));
-                            // Your code
-                        } else if (type.equals("Network")) {
-                            int targetUid = StringUtils.StringToInteger(StringUtils.getSubString(data, "target=", ","));
-                            String proto = StringUtils.getSubString(data, "proto=", ";");
-                            // Your code
+                        } catch (Exception e) {
+                            // 出现异常
                         }
                     }
-                } catch (ErrnoException | InterruptedIOException | NumberFormatException ignored) {
-
-                } catch (Exception e) {
-                    Log.e(e);
                 }
+            } catch (ErrnoException | IOException e) {
+                // 无法连接至ReKernel服务器
+            } catch (Throwable ignored) {
+
             }
-        } catch (ErrnoException | SocketException e) {
-            Log.e("Failed to connect re:kernel server");
-        }
+        });
     }
 }
