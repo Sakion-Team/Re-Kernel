c9846194c0202d22db83938fb8010781c898d86e
Prslc <prslc113@gmail.com>
2025-10-07 15:09:34 +0800

    fix(Integrate): add missing rpc_name and code fields in Binder report format
    The Binder report message in the else branch missed the rpc_name and code
    fields, leading to inconsistent log output compared to the normal path.
    This patch adds the missing arguments to snprintf.
    
    Signed-off-by: Prslc <prslc113@gmail.com>

--- Integrate/rekernel/rekernel.c
+++ Integrate/rekernel/rekernel.c
@@ -277,7 +277,7 @@ void rekernel_report(int reporttype, int type, pid_t src_pid, struct task_struct
 			}
 			snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Binder,bindertype=%s,oneway=%d,from_pid=%d,from=%d,target_pid=%d,target=%d,rpc_name=%s,code=%d;", binder_type[type], oneway, src_pid, task_uid(src).val, dst_pid, task_uid(dst).val, buf, tr->code);
 		} else {
-			snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Binder,bindertype=%s,oneway=%d,from_pid=%d,from=%d,target_pid=%d,target=%d;", binder_type[type], oneway, src_pid, task_uid(src).val, dst_pid, task_uid(dst).val, rpc_type[type], -1);
+			snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Binder,bindertype=%s,oneway=%d,from_pid=%d,from=%d,target_pid=%d,target=%d,rpc_name=%s,code=%d;", binder_type[type], oneway, src_pid, task_uid(src).val, dst_pid, task_uid(dst).val, rpc_type[type], -1);
 		}
 		break;
 	case SIGNAL:
