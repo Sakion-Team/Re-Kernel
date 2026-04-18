3bd547e4cbe278485fe8d396ff3146e6f24faa1e
Timeline <53483352+Nep-Timeline@users.noreply.github.com>
2026-04-18 14:57:51 +0800

    fix typo

--- template/customize.sh
+++ template/customize.sh
@@ -5,7 +5,7 @@ KERNEL_VERSION=$(uname -r)
 ui_print "当前内核版本: $KERNEL_VERSION"
 
 if [[ "$KERNEL_VERSION" == *"6.12"* ]]; then
-    ui_print "检测到内核版本为 android15-6.12"
+    ui_print "检测到内核版本为 android16-6.12"
     cp $MODPATH/lib/android16-6.12-lkmloader.ko $MODPATH/lkm-loader.ko
     cp $MODPATH/lkm/android16-6.12_rekernel.ko $MODPATH/re-kernel.ko
 elif [[ "$KERNEL_VERSION" == *"6.6"* ]]; then
@@ -17,19 +17,19 @@ elif [[ "$KERNEL_VERSION" == *"14"* && "$KERNEL_VERSION" == *"6.1"* ]]; then
     cp $MODPATH/lib/android14-6.1-lkmloader.ko $MODPATH/lkm-loader.ko
     cp $MODPATH/lkm/android14-6.1_rekernel.ko $MODPATH/re-kernel.ko
 elif [[ "$KERNEL_VERSION" == *"14"* && "$KERNEL_VERSION" == *"5.15"* ]]; then
-    ui_print "检测到内核版本为 14-5.15"
+    ui_print "检测到内核版本为 android16-14-5.15"
     cp $MODPATH/lib/android14-5.15-lkmloader.ko $MODPATH/lkm-loader.ko
     cp $MODPATH/lkm/android14-5.15_rekernel.ko $MODPATH/re-kernel.ko
 elif [[ "$KERNEL_VERSION" == *"13"* && "$KERNEL_VERSION" == *"5.15"* ]]; then
-    ui_print "检测到内核版本为 13-5.15"
+    ui_print "检测到内核版本为 android16-13-5.15"
     cp $MODPATH/lib/android13-5.15-lkmloader.ko $MODPATH/lkm-loader.ko
     cp $MODPATH/lkm/android13-5.15_rekernel.ko $MODPATH/re-kernel.ko
 elif [[ "$KERNEL_VERSION" == *"13"* && "$KERNEL_VERSION" == *"5.10"* ]]; then
-    ui_print "检测到内核版本为 13-5.10"
+    ui_print "检测到内核版本为 android16-13-5.10"
     cp $MODPATH/lib/android13-5.10-lkmloader.ko $MODPATH/lkm-loader.ko
     cp $MODPATH/lkm/android13-5.10_rekernel.ko $MODPATH/re-kernel.ko
 elif [[ "$KERNEL_VERSION" == *"12"* && "$KERNEL_VERSION" == *"5.10"* ]]; then
-    ui_print "检测到内核版本为 12-5.10"
+    ui_print "检测到内核版本为 android16-12-5.10"
     cp $MODPATH/lib/android12-5.10-lkmloader.ko $MODPATH/lkm-loader.ko
     cp $MODPATH/lkm/android12-5.10_rekernel.ko $MODPATH/re-kernel.ko
 else
